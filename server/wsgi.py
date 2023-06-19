e0e8a797bf3405a912646882c44d165eb7038964
Tarrin Curtis <CurtisT1@cardiff.ac.uk>
2023-06-19 20:50:35 +0100

    replaced app.py with wsgi.py for gunicorn

--- server/wsgi.py
+++ server/wsgi.py
@@ -4,7 +4,7 @@ from server import server_blueprint
 
 def run_app():
   app.register_blueprint(server_blueprint, url_prefix="/api")
-  app.run(debug=True, port=os.environ['PORT'] or 8080)
+  app.run(debug=False, port=os.environ.get('PORT') or 8080)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   run_app()
