768aa0803c9f6e5667efef6ea9da1a1197623171
Tarrin <tarrinc3@gmail.com>
2023-06-20 19:48:37 +0100

    removed admin creation route for security purposes

--- server/models/AdminModel.py
+++ server/models/AdminModel.py
@@ -1,7 +1,4 @@
 from db.Schema import Admin
-from utils.HashPassword import hash_password
-import settings
-
 from sqlalchemy import exc
 from middleware.Authentication import generate_auth_token
 from CustomExceptions.DBException import DBException
@@ -21,20 +18,6 @@ def login_handler(email, password):
     return (auth_token, admin_data)
   except exc.SQLAlchemyError:
     raise DBException("Failed to check login credentials. Try again.", 500)
-  except Exception as e:
-    if type(e) is not DBException:
-      raise DBException("Something went wrong. Please contact our team if this continues.", 500)
-    else:
-      raise e
-  
-def create_admin_handler(email, password):
-  try:
-    hash = hash_password(password)
-    new_admin = Admin(email=email, hash=hash)
-    settings.db.session.add(new_admin)
-    settings.db.session.commit()
-  except exc.SQLAlchemyError:
-    raise DBException("Failed to create admin account. Try again.", 500)
   except Exception as e:
     if type(e) is not DBException:
       raise DBException("Something went wrong. Please contact our team if this continues.", 500)
--- server/routes/AdminRoute.py
+++ server/routes/AdminRoute.py
@@ -1,6 +1,6 @@
 from CustomExceptions.DBException import DBException
 from flask import Blueprint, request, Response
-from models.AdminModel import login_handler, create_admin_handler
+from models.AdminModel import login_handler
 import json
 import datetime
 from settings import limiter
@@ -18,19 +18,6 @@ def login():
     resp = Response(json.dumps(result[1]), status=200, mimetype="application/json")
     resp.set_cookie(key="auth_token", value=result[0], expires=datetime.datetime.utcnow() + datetime.timedelta(hours=2), httponly=True, samesite="Lax")
     return resp
-  except DBException as e:
-    return Response(e.message, status=e.status_code, mimetype="application/json")
-  except Exception as e:
-    return Response(str(e), status=500, mimetype="application/json")
-  
-@admin_blueprint.route('/create', methods=["POST"])
-def create_admin():
-  email = request.json.get("email")
-  password = request.json.get("password")
-
-  try:
-    create_admin_handler(email, password)
-    return Response("Admin account created successfully.", status=201, mimetype="application/json")
   except DBException as e:
     return Response(e.message, status=e.status_code, mimetype="application/json")
   except Exception as e:
