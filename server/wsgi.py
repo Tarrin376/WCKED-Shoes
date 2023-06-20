3ac43c2df32e9b9db152febd4aca24be3b5b711f
Tarrin <tarrinc3@gmail.com>
2023-06-20 12:20:33 +0100

    changed to gunicorn WSGI container

--- server/wsgi.py
+++ server/wsgi.py
@@ -3,8 +3,16 @@ from settings import app
 from server import server_blueprint
 
 def run_app():
-  app.register_blueprint(server_blueprint, url_prefix="/api")
-  app.run(debug=False, port=os.environ.get('PORT') or 8080)
+  port = int(os.environ.get('PORT', 8080))
+  mode = os.environ['FLASK_DEBUG']
 
-if __name__ == '__main__':
-  run_app()
+  if mode == 'development':
+    app.register_blueprint(server_blueprint, url_prefix="/api")
+    app.run(debug=True, port=port)
+  else:
+    app.register_blueprint(server_blueprint, url_prefix="/api")
+    bind = f"0.0.0.0:{port}"
+    cmd = f"gunicorn --bind {bind} wsgi:app"
+    os.system(cmd)
+
+run_app()
