0c048349a05aa4db7b2ce4c9f5502358b4b2d7b7
Tarrin <tarrinc3@gmail.com>
2023-06-21 20:25:04 +0100

    change

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,7 +8,11 @@ def run_app():
   cmd = f"gunicorn --bind {bind} wsgi:app"
   os.system(cmd)
 
+def handle_request(environ, start_response):
+  return app(environ, start_response)
+
 app.register_blueprint(server_blueprint, url_prefix="/api")
 
 if __name__ == '__main__':
+  callable = handle_request
   run_app()
