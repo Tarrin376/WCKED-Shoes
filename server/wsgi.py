8fbdd419727478e090b209dfd99b61453753f0a9
Tarrin <tarrinc3@gmail.com>
2023-06-22 18:31:03 +0100

    change

--- server/wsgi.py
+++ server/wsgi.py
@@ -9,13 +9,10 @@ def run_app():
   cmd = f"gunicorn --bind {bind} wsgi:app"
   os.system(cmd)
 
-def handle_request(environ, start_response):
-  return app(environ, start_response)
-
 app.register_blueprint(server_blueprint, url_prefix="/api")
 
-@app.route('https://wcked-shoes.onrender.com/', defaults={'u_path': ''})
-@app.route('https://wcked-shoes.onrender.com/<path:u_path>')
+@app.route('/', defaults={'u_path': ''})
+@app.route('/<path:u_path>')
 def catch_all(u_path):
   if not u_path.startswith('/api'):
     return send_file(f"{app.static_folder}/index.html")
@@ -23,5 +20,4 @@ def catch_all(u_path):
   return Response("API route not found", status=404, mimetype="application/json")
 
 if __name__ == '__main__':
-  callable = handle_request
   run_app()
