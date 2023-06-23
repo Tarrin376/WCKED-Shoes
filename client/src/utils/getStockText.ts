ff29c6274f4d13d72c8f0fc7e23fcfaa85f0c21f
Tarrin <tarrinc3@gmail.com>
2023-06-23 10:20:50 +0100

    shortened 'Only x left in stock' to 'Only x left' to prevent UI problems with small product cards

--- client/src/utils/getStockText.ts
+++ client/src/utils/getStockText.ts
@@ -5,7 +5,7 @@ export const getStockText = (stock: number | undefined) => {
   }
   
   if (stock <= 10) {
-    return { color: "dark:text-limited-stock-orange text-[#fb8f1d]", message: `Only ${stock} left in stock` };
+    return { color: "dark:text-limited-stock-orange text-[#fb8f1d]", message: `Only ${stock} left` };
   } else {
     return {color: "dark:text-in-stock-green-text-dark text-in-stock-green-text", message: "In stock" };
   }
